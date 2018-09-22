4f8537db2eaf7db27e779fa01172707ca9aef193
Chai Feng <chaifeng@chaifeng.com>
2018-09-22 09:02:17 +0800

    add docker-ce 18.06

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -xe
 export BENTO_UBUNTU_VERSION=201806.08.0
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
 
+BENTO_UBUNTU=ubuntu-18.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
+BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
+BENTO_UBUNTU=ubuntu-14.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-18.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=17.12 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
