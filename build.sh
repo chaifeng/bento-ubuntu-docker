06b342dc920b396297b2e0daba6ed0a5c5bb2578
Chai Feng <chaifeng@chaifeng.com>
2018-09-25 15:52:31 +0800

    don't build docker 17.12 anymore

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 set -xe
 
-export BENTO_UBUNTU_VERSION=201808.24.0
-export VAGRANT_DEFAULT_PROVIDER=virtualbox
-
 BENTO_UBUNTU=ubuntu-18.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-14.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-18.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
-BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=17.12 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-16.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
-BENTO_UBUNTU=ubuntu-17.10 DOCKER_VERSION=17.12 bash -x ./build-box.sh
-BENTO_UBUNTU=ubuntu-17.10 DOCKER_VERSION=18.03 bash -x ./build-box.sh
-BENTO_UBUNTU=ubuntu-14.04 DOCKER_VERSION=17.12 bash -x ./build-box.sh
 BENTO_UBUNTU=ubuntu-14.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
