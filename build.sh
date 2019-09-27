bc708252747fff14cdc252c93a56cf5a702fc3d5
Chai Feng <chaifeng@chaifeng.com>
2019-09-27 15:59:20 +0200

    Using docker v19.03

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
-BENTO_BOX=debian-9.6 DOCKER_VERSION=18.09 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-18.04 DOCKER_VERSION=18.09 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-16.04 DOCKER_VERSION=18.09 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-18.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-16.04 DOCKER_VERSION=18.06 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-18.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
-BENTO_BOX=ubuntu-16.04 DOCKER_VERSION=18.03 bash -x ./build-box.sh
+export DOCKER_VERSION=19.03
+BENTO_BOX=debian-9.6 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-16.04 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-16.04 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
+BENTO_BOX=ubuntu-16.04 bash -x ./build-box.sh
