c35ac8616649f19dcdb129a7da710f9a14a4bf6d
Chai Feng <chaifeng@chaifeng.com>
2019-11-19 10:42:29 +0100

    Adding docker 19.03.3/5

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 set -xe
 
 export DOCKER_VERSION=19.03
+export BENTO_BOX_VERSION="201906.18.0"
+
+DOCKER_VERSION=19.03.3 BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
+DOCKER_VERSION=19.03.5 BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
 
 BENTO_BOX=debian-9.6 BENTO_BOX_VERSION="201812.27.0" bash -x ./build-box.sh
 BENTO_BOX=debian-9.8 BENTO_BOX_VERSION="201906.17.0" bash -x ./build-box.sh
 BENTO_BOX=debian-10 BENTO_BOX_VERSION="201907.07.0" bash -x ./build-box.sh
 
-export BENTO_BOX_VERSION="201906.18.0"
 BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
 BENTO_BOX=ubuntu-16.04 bash -x ./build-box.sh
