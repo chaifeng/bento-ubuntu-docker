fe82f5ae3e70962f1b6eb197e6acdbf14d4443b2
Chai Feng <chaifeng@chaifeng.com>
2023-08-06 11:19:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,52 @@
 #!/usr/bin/env bash
 set -xe
 
+declare -A bento_box_versions
+bento_box_versions[ubuntu-23.04-arm64]=202306.28.0
+bento_box_versions[ubuntu-22.10-arm64]=202306.30.0
+bento_box_versions[ubuntu-22.04-arm64]=202306.30.0
+bento_box_versions[debian-12-arm64]=202306.13.0
+bento_box_versions[debian-11-arm64]=202306.28.0
+bento_box_versions[debian-10-arm64]=202306.28.0
+
+bento_box_versions[ubuntu-23.04]=202304.25.0
+bento_box_versions[ubuntu-22.10]=202304.25.0
+bento_box_versions[ubuntu-22.04]=202304.25.0
+bento_box_versions[debian-12]=202306.13.0
+bento_box_versions[debian-11]=202303.13.0
+bento_box_versions[debian-10]=202304.28.0
+
+
 if [[ "$(uname -m)" = arm64 ]]; then
-    DOCKER_VERSION=24.0.5 VAGRANT_DEFAULT_PROVIDER=parallels BENTO_BOX=ubuntu-22.04 BENTO_BOX_VERSION=202306.28.0 bash -x ./build-box.sh
-    exit
+    bento_box_name_suffix="-arm64"
+    declare -a providers=(parallels)
+else
+    bento_box_name_suffix=""
+    declare -a providers=(
+        virtualbox
+        vmware_desktop
+        parallels
+    )
+
 fi
 
-export DOCKER_VERSION=19.03.13
-
-declare -a providers=(
-  parallels
-  #virtualbox
-  #vmware_desktop
+declare -a docker_versions=(
+    "24.0.5"
+    "23.0.0"
 )
-for provider in "${providers[@]}"; do
-  #VAGRANT_DEFAULT_PROVIDER=$provider BENTO_BOX_VERSION="202103.19.0" BENTO_BOX=ubuntu-21.04 DOCKER_VERSION=20.10.2 bash -x ./build-box.sh
-  VAGRANT_DEFAULT_PROVIDER=$provider BENTO_BOX_VERSION="202012.23.0" BENTO_BOX=ubuntu-20.04 bash -x ./build-box.sh
-  VAGRANT_DEFAULT_PROVIDER=$provider BENTO_BOX_VERSION="202012.21.0" BENTO_BOX=ubuntu-18.04 bash -x ./build-box.sh
 
-  VAGRANT_DEFAULT_PROVIDER=$provider BENTO_BOX_VERSION="202102.02.0" BENTO_BOX=debian-9 bash -x ./build-box.sh
-  VAGRANT_DEFAULT_PROVIDER=$provider BENTO_BOX_VERSION="202102.10.0" BENTO_BOX=debian-10 bash -x ./build-box.sh
+declare -a bento_boxes=(
+    "ubuntu-23.04"
+    "ubuntu-22.04"
+    "debian-12"
+    "debian-11"
+)
+
+for provider in "${providers[@]}"; do
+  for docker_version in "${docker_versions[@]}"; do
+    for bento_box in "${bento_boxes[@]}"; do
+      VAGRANT_DEFAULT_PROVIDER="$provider" BENTO_BOX="$bento_box" BENTO_BOX_VERSION="${bento_box_versions[${bento_box}${bento_box_name_suffix}]:?undefined box version}" DOCKER_VERSION="${docker_version}" bash -x ./build-box.sh
+    done
+  done
 done
 
